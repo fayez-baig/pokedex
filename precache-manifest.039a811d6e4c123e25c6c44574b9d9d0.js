26897fb21af4cd674c94beaf18ec14eec6792497
fayez baig <coderninja16@gmail.com>
2020-05-22 16:06:30 +0530

    Updates

---
+++ precache-manifest.039a811d6e4c123e25c6c44574b9d9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f565bcff27da6bec4f51f4faa101c79",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "96097284381b7c52d3d8",
+    "url": "/pokedex/static/css/main.9ac86071.chunk.css"
+  },
+  {
+    "revision": "a041565f949ffbf8e421",
+    "url": "/pokedex/static/js/2.b5c1536f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.b5c1536f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96097284381b7c52d3d8",
+    "url": "/pokedex/static/js/main.30e530c5.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  }
+]);
